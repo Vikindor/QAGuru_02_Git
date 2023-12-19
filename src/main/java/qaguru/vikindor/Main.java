fc5b469e4bfaddf447494775f43c24173924ca91
Vikindor <Vikindor@users.noreply.github.com>
2023-12-20 00:13:50 +0100

    Update Main.java
    added msg

--- src/main/java/qaguru/vikindor/Main.java
+++ src/main/java/qaguru/vikindor/Main.java
@@ -1,8 +1,11 @@
 package qaguru.vikindor;
 
 public class Main {
+
+    private static String msg = ""Hello QA Guru!!!""
+    
     public static void main(String[] args) {
-        System.out.printf("Hello QA Guru!!!");
+        System.out.println(msg);
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
