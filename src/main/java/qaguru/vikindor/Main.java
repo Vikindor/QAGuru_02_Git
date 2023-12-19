d6fe633e9b9586ffe779cbaeff4145e6a39691c7
Vikindor <Vikindor@users.noreply.github.com>
2023-12-20 00:35:38 +0100

    Update Main.java
    changed typo in msg

--- src/main/java/qaguru/vikindor/Main.java
+++ src/main/java/qaguru/vikindor/Main.java
@@ -2,7 +2,7 @@ package qaguru.vikindor;
 
 public class Main {
 
-    private static String msg = ""Hello QA Guru!!!""
+    private static String msg = "Hello QA Guru!"
     
     public static void main(String[] args) {
         System.out.println(msg);
